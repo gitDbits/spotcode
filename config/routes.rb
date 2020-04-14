f0bf37ade034fffbec33006b7d7e40a7b9b8b943
gitDbits <dbs.noronha@gmail.com>
2020-04-13 21:12:39 -0400

    Create favorites songs, albums, artists

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "home#index"
+  root 'home#index'
+
+  concern :favoritable do |options|
+    shallow do
+      post '/favorite', { to: "favorites#create", on: :member }.merge(options)
+      delete '/favorite', { to: "favorites#destroy", on: :member }.merge(options)
+    end
+  end
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do 
@@ -9,6 +16,19 @@ Rails.application.routes.draw do
       resources :search, only: :index
       resources :albums, only: :show do 
         resources :recently_heards, only: :create
+      end
+      resources :favorites, only: :index
+
+      resources :songs, only: [] do
+        concerns :favoritable, favoritable_type: 'Song'
+      end
+
+      resources :artists, only: [] do
+        concerns :favoritable, favoritable_type: 'Artist'
+      end
+
+      resources :albums, only: [] do
+        concerns :favoritable, favoritable_type: 'Album'
       end
     end
   end
