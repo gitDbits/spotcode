f3c35ad23c298c1f3108d9084bc96f0653f9b587
gitDbits <dbs.noronha@gmail.com>
2020-04-17 16:19:40 -0400

    Add bulma component and default import app js

---
+++ app/javascript/screens/favorites/index.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const FavoriteScreen = () => {
+  return(
+    <Fragment>
+      <div>Favorite Screen </div>
+    </Fragment>
+  )
+}
+
+export default FavoriteScreen;
