5a1f63ee4a2c740f8eb3cfbc2c7558423925e681
gitDbits <dbs.noronha@gmail.com>
2020-04-17 09:38:41 -0400

    Install webpack react rails

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,13 +1,14 @@
 class Category < ApplicationRecord
   has_many :albums
+
   validates :name, presence: true
   has_one_attached :image
 
   def artists
-    Artist.joins(:albums).where(albums: { id: self.albums.ids }).distinct
+    Artist.joins(:albums).where(albums: { id: albums.ids }).distinct
   end
-  
+
   def songs
-    Song.joins(:album).where(songs: { id: self.albums.ids }).distinct
+    Song.joins(:album).where(songs: { id: albums.ids }). distinct
   end
 end
