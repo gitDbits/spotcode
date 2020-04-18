64f34e14b187148090ac19a3662e42f3536736e9
gitDbits <dbs.noronha@gmail.com>
2020-04-17 22:24:32 -0400

    Done Navbar and action log in user

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -3,13 +3,15 @@ import 'react-bulma-components/dist/react-bulma-components.min.css';
 import 'app.scss';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
+import Menu from './common/menu';
  
  
 const App = () => {
   return(
     <Fragment>
       <BrowserRouter>
-        <Routes/>
+        <Menu />
+        <Routes />
       </BrowserRouter>
     </Fragment>
   );
